2c4a26eb742966632e1dbbde38b7a37967284a33
melpereira7 <43989969+melpereira7@users.noreply.github.com>
2019-05-09 15:33:15 +0100

    Add files via upload

--- ag.c
+++ ag.c
@@ -7,6 +7,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <time.h>
 
 #define MAX 512
 #define FIFO "/tmp/so"
@@ -14,29 +15,53 @@
 struct venda
 {
 	int cod;
-	int qtdVendas;
-	int montante;
-}
+	int qtd;
+	double montante;
+};
 typedef struct venda* mVenda;
 
 int main(){
 
-	char* linhaVenda;
-	mVenda* vendas;
-	while(read(0, linhaVenda, MAX) != eof){
-		venda[i]->cod = strtok(linhaVenda, " ");
-		venda[i]->qtd = strtok(NULL, " ");
-		venda[i]->montante = strtok(NULL, " ");
-		i++;
-	}
+	char* linhaVenda = malloc(sizeof(char*)*MAX);
+	mVenda* venda = malloc(sizeof(struct venda)*MAX);
+	mVenda vendaAux = malloc(sizeof(struct venda));
+	char* vendaAg = malloc(sizeof(char*)*MAX);
+	int flag=0, i=1, j=0, k=0;
+	time_t now;
+	struct tm *mytime = localtime(&now); 
 
-	nVendasAg = i;
-	for(i=0; i<nVendasAg; i++){
-		for(j=0; j<nVendasAg; j++){
-			if(venda[i]->cod == venda[j]->cod){
-				
+	read(0, linhaVenda, MAX);
+	do{
+		vendaAux->cod = atoi(strtok(linhaVenda, " "));
+		vendaAux->qtd = atoi(strtok(NULL, " "));
+		vendaAux->montante = atof(strtok(NULL, " "));
+
+		flag = 0;
+		for(j=0; j<(i-1); j++){
+			if(venda[j] != NULL && vendaAux->cod == venda[j]->cod)
+			{
+				venda[j]->qtd += vendaAux->qtd;
+				venda[j]->montante += venda[j]->montante;
+				flag = 1;
+				j=i-1;
 			}
+		i++;
 		}
+		if(flag == 0)
+		{
+			venda[i-1]->cod = vendaAux->cod;
+			venda[i-1]->qtd = vendaAux->qtd;
+			venda[i-1]->montante = vendaAux->montante; 
+		}
+	}while(read(0, linhaVenda, MAX) != 1);
+
+	for (int k = 0; k < (i-1); k++)
+	{
+		snprintf(vendaAg, MAX, "%d %d %f", venda[k]->cod, venda[k]->qtd, venda[k]->montante);
+		write(1,vendaAg, strlen(vendaAg));
 	}
 
+//snprintf(nomeFich, MAX, "%d-%d-%dT%d:%d:%d", mytime->tm_year+1900, mytime->tm_mon+1, mytime->tm_mday, mytime->tm_hour, mytime->tm_min, mytime->tm_sec);
+
+//fAg = open(nomeFich, O_CREAT, O_WRONLY);	
 }
